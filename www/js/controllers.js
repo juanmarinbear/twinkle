9492f02d0ad453054b018080cca2c124bdd69b80
Juan Marin <juan@codecery.com>
2014-03-11 14:48:51 -0700

    Simplifies controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,10 +3,12 @@ angular.module('starter.controllers', [])
 .controller('SignUpFormCtrl',
   ['$rootScope', '$scope', '$location', 'Auth', function($rootScope, $scope, $location, Auth) {
     $scope.signUp = function(){
-      Auth.register({
-        username: $scope.username,
-        email: $scope.email,
-        password: $scope.password},
+      Auth.signUp(
+        {
+          username: $scope.username,
+          email: $scope.email,
+          password: $scope.password
+        },
         function(user) {
           console.log('Sign Up Success!');
           console.log(user);
