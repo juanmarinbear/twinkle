f9b4e22c65287bce038504100f1fabd9cd907724
Juan Marin <juan@codecery.com>
2014-03-11 14:49:08 -0700

    Simplifies Sign Up service

--- www/js/services.js
+++ www/js/services.js
@@ -1,29 +1,19 @@
 angular.module('starter.services', [])
 
 .factory('Parse', function() {
+
   Parse.initialize("bI2DrpfOupDiOP6vsMG5JuqPc6Uf8VtREKkA7tFk", "MuWjOV8pYJuQZCmexYhq7nc60R1aqKBUwN2iOM5j");
   return Parse;
 })
 
-.factory('Storage', function() {
-  var storage = window.localStorage;
-  return storage;
-})
-
-.factory('Auth', ['$http', 'Parse', 'Storage', function($http, Parse, Storage) {
-  // var currentUser = $cookieStore.get('user') || { username: '' };
-
-  // $cookieStore.remove('user');
+.factory('Auth', ['$http', 'Parse', function($http, Parse) {
 
   return {
-    isLoggedIn: function(user) {
-    },
-    register: function(user, success, error) {
+
+    signUp: function(user, success, error) {
       var parseUser = new Parse.User(user);
-      parseUser.signUp(null, {
-        success: success,
-        error: error
-      });
-    }
+      parseUser.signUp(null, { success: success, error: error });
+    },
+
   }
 }]);
