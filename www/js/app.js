a48581169329a43a869329da1afb175dc55e2cd5
Juan Marin <juan@codecery.com>
2014-03-11 18:55:04 -0700

    Removes unnecessary code

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,7 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.services', 'starter.controllers'])
 
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider ) {
 
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
 
     // Sign Up / Sign In Pattern
@@ -42,10 +30,10 @@ angular.module('starter', ['ionic', 'starter.services', 'starter.controllers'])
 
     .state('user', {
       url: '/',
-      templateUrl: 'tempalteUrl/home.html'
+      templateUrl: 'templates/home.html'
     });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/launch');
 
-});
+}]);
 
